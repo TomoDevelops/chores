f9b77e4579bdc1b4883677082471cf0dd039fd30
TomoDevelops <dev@tomodevelops.com>
2024-08-06 15:42:51 +0900

    refactored config variable

--- components/custom/AuthForm.tsx
+++ components/custom/AuthForm.tsx
@@ -5,44 +5,40 @@ import { useSignUp } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
 // Zod
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-// Custom Validation
+// Custom Validation & Config
 import { authSchema, verificationSchema } from "@/validation/auth.schema";
+import { AuthFormConfig } from "@/config/auth/authFormConfig";
 
 // UI
 import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
+    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 function AuthForm({ formType }: { formType: "signup" | "signin" }) {
     const { isLoaded, signUp, setActive } = useSignUp();
     const [verifying, setVerifying] = useState(false);
     const router = useRouter();
 
-    const verificationForm = useForm<z.infer<typeof verificationSchema>>({
-        resolver: zodResolver(verificationSchema),
-        defaultValues: {
-            verificationCode: "",
-        },
-    });
-
-    const authForm = useForm<z.infer<typeof authSchema>>({
-        resolver: zodResolver(authSchema),
-        defaultValues: {
-            email: "",
-            password: "",
-        },
-    });
+    const { verificationForm, authForm } = AuthFormConfig();
 
     // onSubmit Handler
     const onSubmit = async (values: z.infer<typeof authSchema>) => {
@@ -98,71 +94,99 @@ function AuthForm({ formType }: { formType: "signup" | "signin" }) {
     // Verification Code Form
     if (verifying) {
         return (
-            <Form key="verification-form" {...verificationForm}>
-                <form
-                    onSubmit={verificationForm.handleSubmit(onVerify)}
-                    className="space-y-8 w-10/12 xl:w-1/3"
-                >
-                    <FormField
-                        control={verificationForm.control}
-                        name="verificationCode"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>認証コード</FormLabel>
-                                <FormControl>
-                                    <Input {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button type="submit">認証する</Button>
-                </form>
-            </Form>
+            <Card className="w-3/4 xl:w-1/4 py-8">
+                <CardContent>
+                    <Form key="verification-form" {...verificationForm}>
+                        <form
+                            onSubmit={verificationForm.handleSubmit(onVerify)}
+                            className="space-y-8"
+                        >
+                            <FormField
+                                control={verificationForm.control}
+                                name="verificationCode"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>認証コード</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit">認証する</Button>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
         );
     }
 
     // Main Authentication Code Form
     return (
-        <Form key="auth-form" {...authForm}>
-            <form
-                onSubmit={authForm.handleSubmit(onSubmit)}
-                className="space-y-8 w-10/12 xl:w-1/3"
-            >
-                <FormField
-                    control={authForm.control}
-                    name="email"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>メールアドレス</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="example@TomoDevelops.com"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={authForm.control}
-                    name="password"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>パスワード</FormLabel>
-                            <FormControl>
-                                <Input type="password" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                />
-                <Button type="submit">
-                    {formType === "signup" ? "登録" : "ログイン"}
-                </Button>
-            </form>
-        </Form>
+        <Card className="w-3/4 xl:w-1/4 py-8">
+            <CardHeader>
+                <CardTitle>
+                    {formType === "signup" ? "アカウント登録" : "ログイン"}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <Form key="auth-form" {...authForm}>
+                    <form
+                        onSubmit={authForm.handleSubmit(onSubmit)}
+                        className="space-y-8 "
+                    >
+                        <FormField
+                            control={authForm.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>メールアドレス</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            placeholder="example@TomoDevelops.com"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={authForm.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>パスワード</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" {...field} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit" className="px-10">
+                            {formType === "signup" ? "登録" : "ログイン"}
+                        </Button>
+                        <FormDescription>
+                            {formType === "signup"
+                                ? `アカウントを持っている方は`
+                                : `アカウントを持っていない方は`}
+                            <Link
+                                href={
+                                    formType === "signup"
+                                        ? "/signin"
+                                        : "/signup"
+                                }
+                                className="underline text-blue-400"
+                            >
+                                こちら
+                            </Link>
+                        </FormDescription>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
     );
 }
 
---
+++ config/auth/authFormConfig.ts
@@ -0,0 +1,23 @@
+import { authSchema, verificationSchema } from "@/validation/auth.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export const AuthFormConfig = () => {
+    const verificationForm = useForm<z.infer<typeof verificationSchema>>({
+        resolver: zodResolver(verificationSchema),
+        defaultValues: {
+            verificationCode: "",
+        },
+    });
+
+    const authForm = useForm<z.infer<typeof authSchema>>({
+        resolver: zodResolver(authSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    return { verificationForm, authForm };
+};
