dc7c94166d84f867ce78e4cf0b3e5a791ed0ecad
TomoDevelops MBA <dev@tomodevelops.com>
2024-08-11 22:19:14 +0900

    whitespace

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -9,25 +9,25 @@ import AuthForm from "@/components/custom/auth/AuthForm";
 import AuthFormSkeleton from "@/components/custom/auth/AuthFormSkeleton";
 
 const Signin = () => {
-    const { isLoaded, isSignedIn } = useSession();
-    const router = useRouter();
+  const { isLoaded, isSignedIn } = useSession();
+  const router = useRouter();
 
-    const [showForm, setShowForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-    useEffect(() => {
-        if (isLoaded && !isSignedIn) {
-            setShowForm(true);
-        } else if (isSignedIn) {
-            router.push("/");
-        }
-    }, [isLoaded, isSignedIn, router]);
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      setShowForm(true);
+    } else if (isSignedIn) {
+      router.push("/");
+    }
+  }, [isLoaded, isSignedIn, router]);
 
-    return (
-        <main className="flex min-h-screen w-full items-center justify-center">
-            {!isLoaded && <AuthFormSkeleton />}
-            {showForm && <AuthForm formType="signin" />}
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen w-full items-center justify-center">
+      {!isLoaded && <AuthFormSkeleton />}
+      {showForm && <AuthForm formType="signin" />}
+    </main>
+  );
 };
 
 export default Signin;
